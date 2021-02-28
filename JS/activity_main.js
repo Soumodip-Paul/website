3fec03fc512479b66f6a530c26fde830f850087c
Soumodip Paul <soumodippaul10032002@outlook.in>
2021-02-28 22:40:05 +0530

    commit

--- JS/activity_main.js
+++ JS/activity_main.js
@@ -1,19 +1,7 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAPlG2Zqw3-qLXstp5exVu1Y3-lGfQloI8",
-    authDomain: "spsocialapp.firebaseapp.com",
-    projectId: "spsocialapp",
-    storageBucket: "spsocialapp.appspot.com",
-    messagingSenderId: "781271197094",
-    appId: "1:781271197094:web:08478aa3523f4393315b67",
-    measurementId: "G-Q68122N52H"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
+import  "sign_in.js";
   var user = firebase.auth().currentUser;
   if (user == null) {
-      window.location.replace("./index.html");
+      window.location.replace("./");
   }
   else{
 
--- JS/sign_in.js
+++ JS/sign_in.js
@@ -12,9 +12,9 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-
+  var user = firebase.auth().currentUser;
   function onSignInButtonClick() {
-    var user = firebase.auth().currentUser;
+    
 
     if (user != null) {
       // User is signed in.
