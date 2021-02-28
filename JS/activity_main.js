1c9cb3803eef87e01d65fd1e9e98459b7b70ea2b
Soumodip Paul <soumodippaul10032002@outlook.in>
2021-02-28 23:27:19 +0530

    commit2

--- JS/activity_main.js
+++ JS/activity_main.js
@@ -1,7 +1,26 @@
 import { user } from "sign_in.js";
+import { firebaseConfig } from "sign_in.js";
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
   if (user == null) {
       window.location.replace("./");
   }
-  else{
+  else {
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+  var collectionReferrance = db.collection("users");
+  const entryUser = {
+    UserName: user.displayNmae,
+    uid: user.uid,
+    email = user.email,
+    photoUrl = user.photoURL
+  };
 
+  collectionReferrance.doc(user.uid).set(entryUser).then(() => {
+    console.log("Document successfully written!");
+})
+.catch((error) => {
+    console.error("Error writing document: ", error);
+});
   }
