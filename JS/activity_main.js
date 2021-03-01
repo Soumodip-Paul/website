a2327c87c8706d7d363746c41668f1ba56f1e4e9
Soumodip Paul <soumodippaul10032002@outlook.in>
2021-03-01 15:20:14 +0530

    git

--- JS/activity_main.js
+++ JS/activity_main.js
@@ -1,9 +1,9 @@
-import { user } from "sign_in.js";
-import { firebaseConfig } from "sign_in.js";
-
+import {user} from "./sign_in.js";
   if (user == null) {
       window.location.replace("./");
   }
   else {
     document.getElementById("go").innerHTML=user.displayName;
   }
+  
+  console.log(user);
