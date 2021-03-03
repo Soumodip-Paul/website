0087a73f665936b0061ea8edfc01fd27e1eb1d18
Soumodip Paul <soumodippaul10032002@outlook.in>
2021-03-03 16:11:37 +0530

    commit

--- JS/activity_main.js
+++ JS/activity_main.js
@@ -15,9 +15,23 @@
     }
     var db = firebase.firestore();
     db.collection("posts").get().then((querySnapshot) => {
+      var list="";
       querySnapshot.forEach((doc) => {
           console.log(`${doc.id} => ${doc.data().text}`);
-      });
+          var name_val = doc.data().createdBy.UserName;
+          var id_val = doc.data().text;
+          list+= writeList(name_val,id_val);
+        });
+        $("#post").append(list);
   });
+  
+// db.collection("posts").doc("wxwa9qWbdls3snKsGyt3")
+//     .onSnapshot((doc) => {
+//         console.log("Current data: ", doc.data());
+//     });
+
 }
 
+function writeList(name_val,id_val) {
+  return `${name_val}:  <i> ${id_val} </i><br/><br/> `;
+}
