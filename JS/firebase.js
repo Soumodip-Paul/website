43da09c609c03a25da6eacbb16738e11ef82c2df
Soumodip Paul <soumodippaul10032002@outlook.in>
2021-03-11 23:15:24 +0530

    login feature added

--- JS/firebase.js
+++ JS/firebase.js
@@ -1,33 +1,32 @@
-// Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyAPlG2Zqw3-qLXstp5exVu1Y3-lGfQloI8",
-    authDomain: "spsocialapp.firebaseapp.com",
-    projectId: "spsocialapp",
-    storageBucket: "spsocialapp.appspot.com",
-    messagingSenderId: "781271197094",
-    appId: "1:781271197094:web:08478aa3523f4393315b67",
-    measurementId: "G-Q68122N52H"
-  };
-  // Initialize Firebase
+// This web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyAPlG2Zqw3-qLXstp5exVu1Y3-lGfQloI8",
+  authDomain: "spsocialapp.firebaseapp.com",
+  projectId: "spsocialapp",
+  storageBucket: "spsocialapp.appspot.com",
+  messagingSenderId: "781271197094",
+  appId: "1:781271197094:web:08478aa3523f4393315b67",
+  measurementId: "G-Q68122N52H"
+};
+// Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
   // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
   function googleSignIn() {
       
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth()
-    .signInWithPopup(provider).then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
-  
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-      document.getElementById("auth").innerHTML = user.displayName;
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth()
+  .signInWithPopup(provider).then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+    document.getElementById("auth").innerHTML = user.displayName;
       
     var db = firebase.firestore();
     var collectionReferrance = db.collection("users");
@@ -47,60 +46,53 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 });
   
       
-    }).catch((error) => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-      console.error(error);
-    });
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    console.error(error);
+  });
   }
 
-  function signOut() {
-    firebase.auth().signOut().then(() => {
-      // Sign-out successful.
-      var auth = document.getElementById("auth");
-      if (auth!=null) {
-        auth.innerHTML = "Sign In";
-      }
-      window.location.replace("./");
-    }).catch((error) => {
-      // An error happened.
-      console.error(error);
-    });
-  }
+function signOut() {
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    var auth = document.getElementById("auth");
+    if (auth!=null) {
+    auth.innerHTML = "Sign In";
+    }
+    window.location.replace("./");
+  }).catch((error) => {
+    // An error happened.
+    console.error(error);
+  });
+}
   
-   function getCurrentUser(){
-       return firebase.auth().currentUser;
-   }
-  var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        console.log(authResult)
-        var user = authResult.user;
-        var db = firebase.firestore();
-        var collectionReferrance = db.collection("users");
-        const entryUser = {
-        UserName: user.displayName,
-        uid: user.uid,
-        email: user.email,
-        photoUrl: user.photoURL
-  }
-  collectionReferrance.doc(user.uid).set(entryUser).then(() => {
-    console.log("Document successfully written!");
-    window.location.replace("./mysite.html?uid="+user.uid+"&redirect=1");
-})
-.catch((error) => {
-    console.error("Error writing document: ", error);
-});
-
+function getCurrentUser(){
+    return firebase.auth().currentUser;
+}
+var uiConfig = {
+  callbacks: {
+  signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+  // User successfully signed in.
+  // Return type determines whether we continue the redirect automatically
+  // or whether we leave that to developer to handle.
+    console.log(authResult)
+    $('#email').empty();
+    $('#email').append("<legend>Password</legend><input type=\"password\" name=\"password\" id=\"password\" />");
+    $('#pword').empty();
+    $('#pword').append("<legend>Confirm Password</legend><input type=\"password\" name=\"confirm password\" id=\"confirm-password\"/>");
+    $('#login').text("Save Password")
+    $('#login').click(function () {
+    if ($("#password").val()==$("#confirm-password").val()) {
+        takePassword(authResult,$('#password').val());
+        }
+    });
         return false;
       },
       uiShown: function() {
@@ -129,3 +121,51 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
   // The start method will wait until the DOM is loaded.
   if(document.getElementById("firebaseui-auth-container")!=null){ui.start('#firebaseui-auth-container', uiConfig);}
    export { googleSignIn, signOut , getCurrentUser , ui};
+
+   function login(email,password) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      window.location.replace(`./mysite.html?uid=${user.uid}&redirect=1`);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode,errorMessage);
+    });
+   }
+   function takePassword(authResult,password) {
+      var uid = authResult.user.uid;
+      var user = authResult.user;
+      var db = firebase.firestore();
+      var collectionReferrance = db.collection("users");
+      const entryUser = {
+      UserName: user.displayName,
+      uid: uid,
+      email: user.email,
+      photoUrl: user.photoURL,
+      password : password
+}
+
+var user1 = firebase.auth().currentUser;
+var newPassword = password;
+
+user1.updatePassword(newPassword).then(function() {
+  // Update successful.
+  console.log("success");
+  collectionReferrance.doc(user.uid).set(entryUser).then(() => {
+  console.log("Document successfully written!");
+  window.location.replace("./mysite.html?uid="+user.uid+"&redirect=1");
+})
+.catch((error) => {
+  console.error("Error writing document: ", error);
+});
+}).catch(function(error) {
+  // An error happened.
+});
+
+}
+if(firebase.auth().currentUser==null){$("#login").click(function () {
+  login($('#login_email').val(),$('#password').val());
+});}
