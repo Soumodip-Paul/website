9a3d0f5d146b2aebfcdddb4779cacd7659073b59
Soumodip Paul <soumodippaul10032002@outlook.in>
2021-03-12 18:49:26 +0530

    commit3

--- JS/firebase.js
+++ JS/firebase.js
@@ -86,7 +86,7 @@ uiShown: function() {
       // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
       // firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
