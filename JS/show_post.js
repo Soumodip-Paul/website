96d77ffe5ffcda55243fbe3abc30dad851272b73
Soumodip Paul <soumodippaul10032002@outlook.in>
2021-03-14 12:11:40 +0530

    bug fix 2

--- JS/show_post.js
+++ JS/show_post.js
@@ -1,19 +1,26 @@
 $(function(){
     var db = firebase.firestore();
-    db.collection("posts").get().then((querySnapshot) => {
-      var list="";
+    db.collection("posts").onSnapshot((querySnapshot) => {
+      var list="<div id=\"post\" >";
       querySnapshot.forEach((doc) => {
+          
           console.log(`${doc.id} => ${doc.data().text}`);
           var name_val = doc.data().createdBy.UserName;
           var id_val = doc.data().text;
-          list+= writeList(name_val,id_val);
+          list+= createCard(doc.data());
+          
         });
-        $("#post").append(list);
+        list+="</div>";
+        $("#post").replaceWith(list);
+        list = "";
   });
 });
 function writeList(name_val,id_val) {
   return `${name_val}:  <i> ${id_val} </i><br/><br/> `;
 }
 function  createCard(docData){
-  return ``;
+  return ` <div class="card"><div class="name"><img src="${docData.createdBy.imageUri}" alt="user image" height="33em" width="33em">
+  <span class="postuser"> ${docData.createdBy.UserName}</span></div>
+  <p class="post">${docData.text}</p>
+  <div class="statusbar"></div></div>`;
 }
